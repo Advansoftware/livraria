6a6d7a6a6f7f5b2adef823b043bf93bd8a8978f4
bruno <brunoantunes94@hotmail.com>
2021-12-30 00:02:21 -0300

    fix firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRdudh3sKk1aiTZl8TpLa-R-HVaVtSwuk",
+  authDomain: "react-ebd8c.firebaseapp.com",
+  databaseURL: "https://react-ebd8c-default-rtdb.firebaseio.com",
+  projectId: "react-ebd8c",
+  storageBucket: "react-ebd8c.appspot.com",
+  messagingSenderId: "19573021322",
+  appId: "1:19573021322:web:11e11f45ddb3d612e7ab73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
